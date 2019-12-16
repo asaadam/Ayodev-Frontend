304c34e3728ee50b0a8c638b030e3be5a4c88635
Adam Akbar <graha.asa@gmail.com>
2019-12-16 20:25:18 +0700

    Add Form

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import './HomePage.css';
 
 export default function HomePage() {
 
-  return (<h1>HomePage</h1>)
- 
+
+  let [judul,setJudul]=useState(null);
+
+ async function getData(){
+   let data = 'Halo'
+   data = await Axios.get('http://www.mocky.io/v2/5de11264320000620e809174?mocky-delay=4000ms')
+    setJudul(data.data.Content);
+  }
+
+  getData();
+
+  return (
+    <div>
+    {judul ?   <h1>{judul}</h1> :<div class="loader"></div> }
+      <Link to='/login'>
+        <button >Login</button>
+      </Link>
+      <Link to='/register'>
+        <button>Register</button>
+      </Link>
+    </div>
+  )
+
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
-
+import React, { useState,Fragment } from 'react';
+import './Login.css';
 
 export default function Login() {
 
-  return (<h1>Login</h1>)
+  let [usernameError,setUsernameError]=useState(false);
+  function checkUsername (data){
+    console.log(data.length);
+    if (data.charAt(0) != '@'){
+      setUsernameError(true);
+    }
+     if (data.length === 0){
+      setUsernameError(false);
+    }
+    if (data.charAt(0) == '@'){
+      setUsernameError(false);
+    }
+  }
+
+  return (
+      <div className='body'>
+        <h3>Login</h3>
+        <div className='box'>
+        <form className='form'>
+       {usernameError?<h3 style={{color:'red'}}>Tolong pakai @ ya</h3>:<Fragment/>} 
+        <label >
+        Input your username
+          <input type="text" onChange={data=>checkUsername(data.target.value)}></input>
+          </label>
+          <label>
+          Password
+          <input type="password"></input>
+          </label>
+          <button style={{marginTop:'1rem'}}>Submit</button>
+        </form>
+        </div>
+      </div>
+
+  )
  
 }
 
